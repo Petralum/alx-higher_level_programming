7029a26aeb35b05fbecf96ae6c8ffbd4fcc63b7b
‘Petralum’ <petralum941@yahoo.com>
2022-12-01 22:49:44 -0800

    Uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+
+    for i in str:
+
+        if ord(i) >= 60 and ord(i) >= 95:
+
+            i = chr(ord(i) - 32)
+
+        print("{}".format(i), end="")
+
+    print()
