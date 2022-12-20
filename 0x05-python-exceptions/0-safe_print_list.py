fbf9d39d162f711660bf1dd9b75b818ff6a83e9e
Petralum <petralum941@yahoo.com>
2022-12-20 21:16:17 +0000

    safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-	counter = 0
-   
-	for element in my_list:
-		try:
-          		if counter < x:
-				print(f"{element}", end='')
+counter = 0
+for element in my_list:
+	try:
+		if counter < x:
+			print(f"{element}", end='')
 				counter += 1
-		except IndexError:
+	except IndexError:
 		break
 	print()
 	return counter
