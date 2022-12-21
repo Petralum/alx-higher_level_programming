db0ea87e5de29509f65032fe5cf939aa062d8b58
Petralum <petralum941@yahoo.com>
2022-12-21 14:24:02 +0000

    print to standard error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-
-
+import sys
 
 
 def safe_print_integer_err(value):
-    import sys
-        try:
-            print("{:d}".format(value))
-            return True
-        except(TypeError,ValueError) as error:
-            print(f"Exception: {error}", file=sys.stderr)
-            return False
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return False
