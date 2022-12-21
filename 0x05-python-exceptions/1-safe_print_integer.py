27771cab69c1c5de4dc78fe221dba2607642d653
Petralum <petralum941@yahoo.com>
2022-12-21 11:43:08 +0000

    print interger

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+
+
+
+
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except:
+    except BaseException:
         return False
